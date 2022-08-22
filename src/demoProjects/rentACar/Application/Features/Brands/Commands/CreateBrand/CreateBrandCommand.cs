61662b702079658d17e4f4aa971369ea316dcab0
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-08-22 22:45:34 +0300

    update

--- src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -9,6 +9,7 @@ namespace Application.Features.Brands.Commands.CreateBrand
     public class CreateBrandCommand : IRequest<CreatedBrandDto>
     {
         public string Name { get; set; }
+
         public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandDto>
         {
             private IBrandRepository _brandRepository;
