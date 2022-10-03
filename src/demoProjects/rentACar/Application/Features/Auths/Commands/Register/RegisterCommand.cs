002db752b837ec38884bd3da21a5ca4c06f757da
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-10-03 22:48:48 +0300

    JWT Altyapısı 1

---
+++ src/demoProjects/rentACar/Application/Features/Auths/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,25 @@
+﻿using Application.Features.Auths.Dtos;
+using Application.Features.Auths.Rules;
+using Application.Services.Repositories;
+using Core.Security.Dtos;
+using MediatR;
+
+namespace Application.Features.Auths.Commands.Register
+{
+    public class RegisterCommand :IRequest<RegisterDto>
+    {
+        public UserForRegisterDto UserForRegisterDto { get; set; }
+        public string IpAddress{ get; set; }
+
+        public class RegisterCommandHandler : IRequestHandler<RegisterCommand, RegisterDto>
+        {
+            private readonly AuthBusinessRules _authBusinessRules;
+            private readonly IUserRepository _userRepository;
+          
+            public Task<RegisterDto> Handle(RegisterCommand request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
